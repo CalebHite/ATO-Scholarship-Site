4d038bbd9e281d43fcb3e2988705fa7959259716
CalebHite <96922867+CalebHite@users.noreply.github.com>
2023-09-14 18:45:43 -0500

    First Commit

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPtCjFGj2wisEtIz_NL-I0N9cjhWd30BU",
+  authDomain: "ato-scholarship.firebaseapp.com",
+  projectId: "ato-scholarship",
+  storageBucket: "ato-scholarship.appspot.com",
+  messagingSenderId: "1076172585908",
+  appId: "1:1076172585908:web:736e7c186c0b6a3668d0e1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
